97830dc67f7d12caf43cc05e791016bb690312c3
Jackie Ng <jumpinjackie@gmail.com>
2017-09-13 21:36:25 +1000

    Add build_clean.sh to easily clean out the build dir

---
+++ build_clean.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+rm -rf build/base/centos6/x64/build/scripts
+rm -rf build/base/ubuntu14/x64/build/scripts
+rm -rf build/fdo/centos6/x64/build/scripts
+rm -rf build/fdo/ubuntu14/x64/build/scripts
+rm -rf build/mapguide/centos6/x64/build/scripts
+rm -rf build/mapguide/ubuntu/x64/build/scripts
